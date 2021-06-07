eb67b40beff6092b65cfe7513c0718de33df0470
Kai Mareving <karska.joanna@gmail.com>
2021-06-07 15:34:48 +0200

    Bookings: getData() v.0.1

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,5 @@
-import { select, templates /* , settings, classNames */ } from '../settings.js';
-// import utils from '../utils.js';
+import { select, templates, settings /* , classNames */ } from '../settings.js';
+import utils from '../utils.js';
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
 import HourPicker from './HourPicker.js';
@@ -11,6 +11,46 @@ class Booking {
 
     thisBooking.render(container);
     thisBooking.initWidgets();
+    thisBooking.getData();
+  }
+
+  getData() {
+    const thisBooking = this;
+
+    const startDateParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate);
+    const endDateParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(thisBooking.datePicker.maxDate);
+
+    const params = {
+      booking: [
+        startDateParam,
+        endDateParam,
+      ],
+      eventsCurrent: [
+        settings.db.notRepeatParam,
+        startDateParam,
+        endDateParam,
+      ],
+      eventsRepeat: [
+        settings.db.repeatParam,
+        startDateParam,
+      ],
+    };
+    console.log('getData params:', params);
+
+    const urls = {
+      booking:       'http:' + settings.db.url + '/' + settings.db.bookings
+                                     + '?' + params.booking.join('&'),
+      eventsCurrent: settings.db.url + '/' + settings.db.events
+                                     + '?' + params.eventsCurrent.join('&'),
+      eventsRepeat:  settings.db.url + '/' + settings.db.events
+                                     + '?' + params.eventsRepeat.join('&'),
+    };
+    console.log('getData urls:', urls);
+
+    fetch(urls.booking)
+      .then(function (bookingsResponse) { return bookingsResponse.json(); })
+      .then(function (bookings) { console.log(bookings); });
+
   }
 
   render(container) {
